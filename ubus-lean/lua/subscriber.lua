7ff07411ad55cb967be04e2a94ddfc970eb57173
guang-zhang <johnzhangbkb@gmail.com>
2017-02-22 13:36:14 +0800

    component files

---
+++ ubus-lean/lua/subscriber.lua
@@ -0,0 +1,25 @@
+#!/usr/bin/env lua
+
+--[[
+  A demo of ubus subscriber binding. Should be run after publisher.lua
+--]]
+
+require "ubus"
+require "uloop"
+
+uloop.init()
+
+local conn = ubus.connect()
+if not conn then
+	error("Failed to connect to ubus")
+end
+
+local sub = {
+	notify = function( msg )
+		print("Count: ", msg["count"])
+	end,
+}
+
+conn:subscribe( "test", sub )
+
+uloop.run()
